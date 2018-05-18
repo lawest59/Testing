1d9b4c2e677f7719de12089b7e2e6a2c9c8192d1
lawest59 <lowwest@ucdavis.edu>
2018-05-18 13:39:27 -0700

    Added Code File

---
+++ Git_Test_Code.R
@@ -0,0 +1,18 @@
+####################################################################################################
+## File Name: Git_Test_Code.R
+## Date Created: 18 May 2018
+## Date Updated: 18 May 2018
+## Author: Lowell West
+## Assignment/Project: Git Check
+## Purpose: Figuring Out Git
+####################################################################################################
+
+#### Preamble
+## Setting Working Directory
+
+setwd("/Users/lowellawest/Google Drive/Programming/GIT")
+
+## Loading Packages
+library(car)
+
+####
